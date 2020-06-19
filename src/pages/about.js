22940b7996a0d88e80f79e9d324ef2d5be5faa6f
smithpg <patrickgromersmith@gmail.com>
2020-06-19 00:01:31 -0700

    typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,7 +29,7 @@ const Index = () => {
           <div>
             <Styled.h2 sx={{ my: 1 }}>{'About Me'}</Styled.h2>
             <Styled.p>
-              {`I'm a recent graduate of Oregon Statue Universit's Computer Science program seeking front-end, back-end, or fullstack work.`}
+              {`I'm a recent graduate of Oregon State University's Computer Science program seeking front-end, back-end, or fullstack work.`}
             </Styled.p>
             <Styled.p>
               {`Feel free to drop me a line at patrickgromersmith@gmail.com.`}
